1152cb1221e4f319b354c88cd7e780d5593bb5ed
VedantGanjegaonkar <vedantsg112233@gmail.com>
2024-07-01 15:39:58 +0530

    lazy loading tried

--- cms-frontend/src/app/app-routing.module.ts
+++ cms-frontend/src/app/app-routing.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UploadComponent } from './upload/upload.component';
-import { ContentListComponent } from './page/main/content-list/content-list.component';
-import { SignupComponent } from './authentication/signup/signup.component';
-import { LoginComponent } from './authentication/login/login.component'; 
-import { AuthGuard } from './authentication/auth.guard';
-import { ArticleEditorComponent } from './page/main/article-list/article-editor/article-editor.component';
-import { ArticleListComponent } from './page/main/article-list/article-list.component';
-import { AdminHomeComponent } from './page/admin-home/admin-home.component';
-import { AdminGuard } from './core/authGuards/admin.guard';
-import { NavbarComponent } from './layouts/navbar/navbar.component';
+
 
 const routes: Routes = [
-  // { path: 'upload', component: UploadComponent },
-  // { path: 'home', component: ContentListComponent, canActivate: [AuthGuard] },
+ 
 
-  // {path:'article',component:ArticleEditorComponent},
-  // {path:'articles',component:ArticleListComponent},
-
-  // {path:'adminHome',component:AdminHomeComponent,canActivate: [AdminGuard]},
-
-  // {
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  }
+   // {
   //   path: 'layouts',
   //   component: NavbarComponent,
   //   children: [
@@ -29,17 +19,6 @@ const routes: Routes = [
   //   ]
   // },
 
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  {
-    path: 'auth',
-    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
-  },
-
-
-
-  // {path:'signup', component:SignupComponent},
-  // {path:'login', component:LoginComponent},
-  // { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
 
 ];
 
